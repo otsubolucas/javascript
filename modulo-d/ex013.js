bd29785d0c17d0feeca7693e30589e41b7da1689
Lucas Otsubo <otsubolc@hotmail.com>
2023-03-27 22:17:16 +0900

    Terminei a aula 12 de condições

---
+++ modulo-d/ex013.js
@@ -0,0 +1,15 @@
+var agora = new Date()
+var mes = agora.getMonth()
+mes = 
+console.log('Que Mês é hoje?')
+switch(mes){
+    case 0: 
+        console.log('Janeiro')
+        break
+    case 1: 
+        console.log('Fevereiro')
+        break
+    case 2:
+        console.log('Março')
+        break
+}
