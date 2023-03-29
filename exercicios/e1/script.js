cc7448790c241596f73660f1dd28f1ade3600ddd
Lucas Otsubo <otsubolc@hotmail.com>
2023-03-30 06:20:59 +0900

    Problema no looping do exercício e1

---
+++ exercicios/e1/script.js
@@ -0,0 +1,12 @@
+function contar(){
+    txtin = document.getElementById('inicio')
+    txtf = document.getElementById('fim')
+    txtp = document.getElementById('passo')
+    res = document.querySelector('#res')
+    ini = Number(txtin.value)
+    fim = Number(txtf.value)
+    pas = Number(txtp.value)
+    for(ini; ini <= fim; ini + pas){
+    
+    }
+}
--- modulo-e/ambiente.js
+++ modulo-e/ambiente.js
@@ -1,14 +1,17 @@
-var c = 1    
-do{
-    console.log(`Passo ${c}`)
-    c++
-}while(c <= 10)
+// var c = 1    
+// do{
+//     console.log(`Passo ${c}`)
+//     c++
+// }while(c <= 10)
 
-
-
-
-
+//---------------------------------------------
 // while(c <= 10){
 //     console.log('oi')
 //     c++
 // }
+//-----------------------------------------------
+
+for(var c=1; c<=10; c++){
+    console.log(`Passo ${c}`)
+}
+console.log('FIM')
