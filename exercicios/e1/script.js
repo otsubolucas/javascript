f8068946bc55221433da2651a7340c5e2bc89b0b
Lucas Otsubo <otsubolc@hotmail.com>
2023-04-03 00:02:32 +0900

    MUITOS PROBLEMAS

--- exercicios/e1/script.js
+++ exercicios/e1/script.js
@@ -1,12 +1,19 @@
 function contar(){
-    txtin = document.getElementById('inicio')
-    txtf = document.getElementById('fim')
-    txtp = document.getElementById('passo')
-    res = document.querySelector('#res')
-    ini = Number(txtin.value)
-    fim = Number(txtf.value)
-    pas = Number(txtp.value)
-    for(ini; ini <= fim; ini + pas){
-    
+    var txtin = document.getElementById('inicio')
+    var txtf = document.getElementById('fim')
+    var txtp = document.getElementById('passo')
+    var res = document.querySelector('#res')
+
+    if(txtin.value.length == 0 || txtf.value.length == 0 || txtp.value.length == 0){
+        window.alert('[ERRO] Faltam dados!!')
+    }else{
+
+        res.innerHTML = 'Contando: '
+        var ini = Number(txtin.value)
+        var fim = Number(txtf.value)
+        var pas = Number(txtp.value)
+        for(var c = ini ; c <= fim ; c += pas){
+            res.innerHTML = `${c}`
     }
+}
 }
--- modulo-e/ambiente.js
+++ modulo-e/ambiente.js
@@ -11,7 +11,7 @@
 // }
 //-----------------------------------------------
 
-for(var c=1; c<=10; c++){
+for(var c = 1; c <= 10; c++){
     console.log(`Passo ${c}`)
 }
 console.log('FIM')
