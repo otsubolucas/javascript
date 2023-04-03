af721df3e2db380e9b60890b62d97a1379703f2b
Lucas Otsubo <otsubolc@hotmail.com>
2023-04-04 07:12:37 +0900

    Concertei o BUG, ATENÇÃO AOS SINAIS

--- exercicios/e1/script.js
+++ exercicios/e1/script.js
@@ -2,18 +2,34 @@ function contar(){
     var txtin = document.getElementById('inicio')
     var txtf = document.getElementById('fim')
     var txtp = document.getElementById('passo')
-    var res = document.querySelector('#res')
+    var res = document.getElementById('res')
 
     if(txtin.value.length == 0 || txtf.value.length == 0 || txtp.value.length == 0){
         window.alert('[ERRO] Faltam dados!!')
     }else{
 
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
+
         var ini = Number(txtin.value)
         var fim = Number(txtf.value)
         var pas = Number(txtp.value)
-        for(var c = ini ; c <= fim ; c += pas){
-            res.innerHTML = `${c}`
+        if (pas <= 0){
+            window.alert('Passo inválido! Considerando passo 1!')
+            pas = 1
+        }
+
+        //Contagem crescente
+        if(ini < fim){
+            for(var c = ini ; c <= fim ; c += pas){
+                res.innerHTML += `${c} \u{27A1} `
+            }
+        }else{
+            //Contagem Regressiva
+            for(var c = ini; c >= fim; c-=pas){
+                res.innerHTML += `${c} \u{27A1} `
+            } 
+        }
+
+        res.innerHTML += `\u{1F3C1}`
     }
-}
 }
