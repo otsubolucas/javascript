fef9cb21f9f8fbcf20095424173701cf206c46e1
Lucas Otsubo <otsubolc@hotmail.com>
2023-04-10 23:53:51 +0900

    Terminei Funções

---
+++ modulo-f/funcao01.js
@@ -0,0 +1,9 @@
+//Função para par ou ímpar!
+function parimpar(n){
+    if(n % 2 == 0){
+        return 'Par'
+    }else{
+        return 'Ímpar'
+    }
+}
+console.log(parimpar(11))
---
+++ modulo-f/funcao02.js
@@ -0,0 +1,4 @@
+function soma(n1 = 0,n2 = 0){ //da pra já estabelecer um valor para o parâmetro
+    return n1 + n2
+}
+console.log(soma(1,))
