1bfd0a4df7c56aca5eb0f0b5087c0bd27aa11a1f
Lucas Otsubo <otsubolc@hotmail.com>
2023-04-12 00:54:45 +0900

    Terminei o Exercício de arrays e function

--- exercicios/f1/script.js
+++ exercicios/f1/script.js
@@ -1,15 +1,54 @@
-function adicionar(){
-    var num = document.getElementById('txtn')
-    var n = Number(num.value)
-    var tab = document.getElementById('lista')
-    if(n <= 0 || n > 100){
-        window.alert('Número inválido, Verifique se colocou os número certos!')
+var num = document.getElementById('txtn')
+var res = document.getElementById('res')
+var lista = document.getElementById('flista')
+var valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
     }else{
-        var item = document.createElement('option')
-        item.text = `Valor ${n} adicionado!`
-        tab.appendChild(item)
+        return false
     }
 }
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    }else{
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
 function finalizar(){
-    
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar!')
+    }else{
+        let total = valores.length
+        let maior = Math.max.apply(null, valores)
+        let menor = Math.min.apply(null, valores)
+        let soma = 0
+        for(let pos in valores){
+            soma += valores[pos]
+        }
+        let media = soma/total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+    }
 }
